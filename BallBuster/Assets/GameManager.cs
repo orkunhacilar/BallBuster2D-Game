7f09efb99045c203deb727e32cb37d4b6f456242
Orkun Hacılar <orkun.hacilar@gmail.com>
2023-11-03 07:14:23 +0300

    165 half

--- BallBuster/Assets/GameManager.cs
+++ BallBuster/Assets/GameManager.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class GameManager : MonoBehaviour
 {
+
+    [Header("---LEVEL AYARLAR")]
     public Sprite[] SpriteObjeleri;
     [SerializeField] private GameObject[] Toplar;
+    [SerializeField] private TextMeshProUGUI KalanTopSayisiText;
+    int KalanTopSayisi;
+    int HavuzIndex;
+
+
+    [Header("---TOP ATIS SISTEMI")]
+    [SerializeField] private GameObject TopAtici;
+    [SerializeField] private GameObject TopSoketi;
+    [SerializeField] private GameObject GelecekTop;
+
 
     void Start()
     {
-        
+        KalanTopSayisi = Toplar.Length;
+        TopGetir();
     }
     // 2-Kirmizi
     // 4-Sari
@@ -27,6 +40,30 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.CompareTag("OyunZemini"))
+                {
+                    Vector2 MousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+                    TopAtici.transform.position = Vector2.MoveTowards(TopAtici.transform.position, new Vector2(MousePosition.x, TopAtici.transform.position.y), 30 * Time.deltaTime);
+                }
+            }
+            Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
+        }
+
         
+    }
+
+    void TopGetir()
+    {
+        Toplar[HavuzIndex].transform.SetParent(TopAtici.transform);
+        Toplar[HavuzIndex].transform.position = TopSoketi.transform.position;
+        Toplar[HavuzIndex].SetActive(true);
     }
 }
