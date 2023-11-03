830aa944b52560189f400399e35e54574205d32e
Orkun Hacılar <orkun.hacilar@gmail.com>
2023-11-03 06:27:51 +0300

    165

--- BallBuster/Assets/GameManager.cs
+++ BallBuster/Assets/GameManager.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Sprite[] SpriteObjeleri;
+    [SerializeField] private GameObject[] Toplar;
+
     void Start()
     {
         
     }
+    // 2-Kirmizi
+    // 4-Sari
+    // 8-Yesil
+    // 16-Mavi
+    // 32-Koyu Mavi
+    // 64-Koyu Yesil
+    // 128-Mor
+    // 256-Turuncu
+    // 512-Karisik Sari
+    // 1024
+    // 2048
+
 
     // Update is called once per frame
     void Update()
--- BallBuster/Assets/Top.cs
+++ BallBuster/Assets/Top.cs
@@ -12,15 +12,27 @@ public class Top : MonoBehaviour
     [SerializeField] ParticleSystem BirlesmeEfekt;
     [SerializeField] SpriteRenderer _Renderer;
 
+    bool Birincil;
+
     // Start is called before the first frame update
     void Start()
     {
         SayiText.text = Sayi.ToString();
     }
 
+    void DurumuAyarla()
+    {
+        Birincil = true;
+    }
+
+    void BirincilDurumDegistir()
+    {
+        Invoke("DurumuAyarla", 2f);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag(Sayi.ToString())) //ayni tagde baska top ile carpisirsa
+        if (collision.gameObject.CompareTag(Sayi.ToString()) && Birincil) //ayni tagde baska top ile carpisirsa
         {
             BirlesmeEfekt.Play();
             collision.gameObject.SetActive(false); //carpistigimiz topu kapat
@@ -29,6 +41,39 @@ public class Top : MonoBehaviour
             SayiText.text = Sayi.ToString(); //topumun textini guncelle
             //Sprite degisimi
         }
+
+        switch (Sayi)
+        {
+            case 4:
+                _Renderer.sprite = _GameManager.SpriteObjeleri[1];
+                break;
+            case 8:
+                _Renderer.sprite = _GameManager.SpriteObjeleri[2];
+                break;
+            case 16:
+                _Renderer.sprite = _GameManager.SpriteObjeleri[3];
+                break;
+            case 32:
+                _Renderer.sprite = _GameManager.SpriteObjeleri[4];
+                break;
+            case 64:
+                _Renderer.sprite = _GameManager.SpriteObjeleri[5];
+                break;
+            case 128:
+                _Renderer.sprite = _GameManager.SpriteObjeleri[6];
+                break;
+            case 256:
+                _Renderer.sprite = _GameManager.SpriteObjeleri[7];
+                break;
+            case 512:    
+            case 1024:
+            case 2048:
+                _Renderer.sprite = _GameManager.SpriteObjeleri[8];
+                break;
+        }
+
+        Birincil = false;
+        Invoke("DurumuAyalar", 2f);
     }
 
 }
